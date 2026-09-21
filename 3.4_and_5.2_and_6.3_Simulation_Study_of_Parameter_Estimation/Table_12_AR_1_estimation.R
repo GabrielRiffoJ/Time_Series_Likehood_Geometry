7759186b71caca9939ceb86d82a0c1adf7430b9d
Gabriel Riffo <52945978+GabrielRiffoJ@users.noreply.github.com>
2026-09-21 20:56:35 +0200

    Update Table_12_AR_1_estimation.R

--- 3.4_and_5.2_and_6.3_Simulation_Study_of_Parameter_Estimation/Table_12_AR_1_estimation.R
+++ 3.4_and_5.2_and_6.3_Simulation_Study_of_Parameter_Estimation/Table_12_AR_1_estimation.R
@@ -1,11 +1,10 @@
 
-
+set.seed(1234)
 muestras=c()
 for (i in c(1:500)){
   arma_sim <- arima.sim(n = 15, model = list(ar = 0.5))
   muestras=rbind(muestras,arma_sim)
 }
-
 
 resultado=c()
 
@@ -13,72 +12,37 @@ for (i in c(1:500) ){
   Y=muestras[i,]
   n=length(Y)
 
-
   fobj<-function(par){
-    a0=par[1]
-    a1=par[2]
-    a2=par[3]
+    phi=par[1]
+    s2=par[2]
+    if (abs(phi)>=0.999 || s2<=0) return(1e10)
     M=matrix(0,n,n)
-    diag(M)=diag(M)+a0^2+a1^2+a2^2
-    M[row(M) == col(M) - 1] <- a0*a1+a1*a2
-    M[row(M) == col(M) - 2] <- a0*a2
-    M[row(M)-1 == col(M)] <- a0*a1+a1*a2
-    M[row(M)-2 == col(M)] <- a0*a2
+    M=s2/(1-phi^2)*phi^abs(row(M)-col(M))
     Minv=chol2inv(chol(M))
     detM=det(M)
     lk=1/2*log(detM)+1/2*t(Y)%*%Minv %*% Y
     return(lk)
   }
 
-  re=optim(c(sd(Y),0,0),fobj)
+  re=optim(c(0,var(Y)),fobj)
 
+  phi=re$par[1]
+  s2=re$par[2]
 
-  a0=re$par[1]
-  a1=re$par[2]
-  a2=re$par[3]
-
-  resultado=rbind(resultado,c(a0,a1,a2))
+  resultado=rbind(resultado,c(phi,s2))
 }
 
 resultado[1,]
 
 colMeans(resultado)
-colMeans(resultado)-c(1,0.5,-0.3)
+colMeans(resultado)-c(0.5,1)
 
 sd(resultado[,1])
 sd(resultado[,2])
-sd(resultado[,3])
-mean(resultado[,1]-1)
-mean(resultado[,2]-0.5)
-mean(resultado[,3]+0.3)
-
-sum((resultado[9,]-c(1,0.5,-0.3))^2)
-
-sum((c(0.1124229,0.0395593,-0.45528))^2)
-
-
-
-
-
-
-#save samples for HomotopyContinuation
+mean(resultado[,1]-0.5)
+mean(resultado[,2]-1)
 
 write.table(muestras, file = "samples.txt", row.names = FALSE, col.names = TRUE, sep = "\t")
-
-
-
-
-#result=c()
-
-a0
-a1
-a2
-
-sum(abs(c(a0-1,a1-0.5, a2+0.3)))
-
-a0^2+a1^2+a2^2
-a0*a1+a1*a2
-a0*a2
 
 
 
